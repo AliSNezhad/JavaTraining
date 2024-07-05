6531a8006108395f5628e1892ab648c8e8bb2480
Ali Sarhang <a.sarhangnezhad@gmail.com>
2024-07-04 22:11:13 -0400

    FizzBuzz added

---
+++ src/FizzBuzz.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class FizzBuzz {
+    private Scanner scanner;
+
+    public  FizzBuzz() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    private void fizzBuzzChecker(int number) {
+        if (number % 3 == 0 && number % 5 == 0) {
+            System.out.println("FizzBuzz");
+        } else if (number % 3 == 0) {
+            System.out.println("Fizz");
+        } else if (number % 5 == 0) {
+            System.out.println("Buzz");
+        } else {
+            System.out.println(number);
+        }
+    }
+
+    public void run() {
+        while (true) {
+        System.out.print("Enter your number to check or q to quit: ");
+        String input = scanner.nextLine();
+
+        if (input.equalsIgnoreCase("q")) {
+            System.out.println("Exiting the program.");
+            break;
+        }
+
+        try {
+            int number = Integer.parseInt(input);
+            fizzBuzzChecker(number);
+
+        } catch (NumberFormatException e) {
+            System.out.println("Enter an Integer value.");
+        }
+    }
+
+        scanner.close();
+}
+
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        FizzBuzz game = new FizzBuzz();
+        game.run();
+    }
+}
