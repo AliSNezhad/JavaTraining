6abfba9b6a3fa07c46a346f94fc92433ac33486f
Ali Sarhang <a.sarhangnezhad@gmail.com>
2024-07-09 20:17:24 -0400

    Array class added

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        FizzBuzz game = new FizzBuzz();
-        game.run();
+        //FizzBuzz game = new FizzBuzz();
+        //game.run();
+        Array ali = new Array();
+        ali.insertFirst(5);
+        ali.insertLast(6);
+        ali.insertLast(7);
+        ali.insertFirst(4);
+        ali.insertAt(0,3);
+        ali.insertFirst(1);
+        ali.insertAt(1,2);
+
+        ali.print();
     }
 }
